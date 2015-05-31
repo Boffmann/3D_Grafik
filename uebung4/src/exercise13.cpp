3ca391870d53b86d3d1deb1324a84a9c4f06098c
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2015-05-31 21:07:50 +0200

    12 mit parabel ohne skalierung

--- uebung4/src/exercise13.cpp
+++ uebung4/src/exercise13.cpp
@@ -71,111 +71,71 @@ QMatrix4x4 Exercise13::applyBallTransformation(const int frame)
     float y = 0.8;
     int length = 1600 * (0.01/fX);
     float distance = (float)(frame%length)/numFramesPerAnimation;
-    float radians = -((360 * distance)/(2.0 * r * M_PI)) * (M_PI/180.0);  // - so it turns the right way
-    float radians_t1 = -((360 * -0.9)/(2.0 * r * M_PI)) * (M_PI/180.0);
-    float max_turn_t1 = -(90-61.61) * (M_PI/180.0);
-    float cosine = cos(radians);
-    float cosine_t1 = cos(radians - radians_t1);
-    float cosine_max_turn_t1 = cos(max_turn_t1);
-    float sine = sin(radians);
-    float sine_t1 = sin(radians - radians_t1);
-    float sine_max_turn_t1 = sin(max_turn_t1);
     float lambda1 = 1.0;
     float lambda2 = 1.0;
     float lambda3 = 1.0;
+    float val;
     float fac;
 
-    // x: -0.9    0.6
-    // y: 0.8     0.8 - r = 0.5
-
-    QVector4D norm(-185.0, -100.0, 0.0, 1.0);
-    norm = norm * (3.0/(50 * sqrt(1769))); // 0.3 langer Normalenvektor auf der Geraden von (-0.9, 0.8) zu (0.1, -1.05)
-    // ich idiot. steigung von -1.85
-    // steigungswinkel von -61.61 grad
-    // d.h. drehung von vektor vom rand der kante zum mittelpunkt der kugel bis 90-61.61 grad drehen danach kann die fallen
-    QVector4D sphere_middle(0.0, 0.3, 0.0, 1.0);
-    QVector4D tmp1(0.0, 0.3, 0.0, 1.0);
-    QVector4D tmp2;
-    const QMatrix4x4 rot_t1({cosine_t1, -sine_t1, 0.0, 0.0,   sine_t1, cosine_t1, 0.0, 0.0,   0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0});
-    const QMatrix4x4 rot_max_turn_t1({cosine_max_turn_t1, -sine_max_turn_t1, 0.0, 0.0,   sine_max_turn_t1, cosine_max_turn_t1, 0.0, 0.0,   0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0});
-    const QMatrix4x4 rotate({cosine, -sine, 0.0, 0.0,   sine, cosine, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0});
-    QMatrix4x4 unit({1.0, 0.0, 0.0, 0.0,    0.0, 1.0, 0.0, 0.0,   0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0});
-
-#if 1
     x += distance;
+    if(x >= 0.10 && !(x > 0.40)) {
+      // stauchung von 0.10 bis 0.40
+      fac = (x - 0.1) / 0.3;
+      val = (1.0-fac) * 1.0 + fac * (d/2.0*r);
+      x = 0.1;
+    } else if(x >= 0.40 && !(x > 0.70)) {
+      // anti stauchung von 0.40 bis 0.70
+      fac = (x - 0.4) / 0.3;
+      val = (1.0-fac) * (d/2.0*r) + fac * 1.0;
+    }
+    if(x >= 0.4)
+      x -= 0.3;
+    // stauchung an achsen der kugel
+    // not sure wie man die lambda berechnen soll
+    // lambda2 = val * sin(winkel);
+    // lambda1 = sqrt(val*val - lambda2 * lambda2);
+    // hat nicht funktioniert
+
+#if 0
     if(x >= 0.10 && !(x > 0.15)) {
       // stauchung von 0.10 bis 0.15
       fac = (x - 0.1) / 0.05;
-      lambda2 = (1.0-fac) * 1.0 + fac * 0.15;
+      lambda2 = (1.0-fac) * 1.0 + fac * (d/2.0*r);
       x = 0.1;
     } else if(x >= 0.15 && !(x > 0.20)) {
       // anti stauchung von 0.15 bis 0.2
       fac = (x - 0.15) / 0.05;
-      lambda2 = (1.0-fac) * 0.15 + fac * 1.0;
+      lambda2 = (1.0-fac) * (d/2.0*r) + fac * 1.0;
     }
     if(x > 0.15) x -= 0.05;
 #endif
 
-    const QMatrix4x4 scale({lambda1, 0.0, 0.0, 0.0,   0.0, lambda2, 0.0, 0.0,   0.0, 0.0, lambda3, 0.0,   0.0, 0.0, 0.0, 1.0});
-
     if(x >= -0.9 && x < 0.1) {  // -0.9 leftcliff / 0.1 behind bottom x
-  #if 1
-      sphere_middle =  rot_t1 * tmp1;
-      if(sphere_middle.x() < 0.142641 && sphere_middle.y() > 0.26392) {
-        // turn till fall at
-        // max x: 0.142641
-        // max y: 0.26392
-      } else {
-        sphere_middle = rot_max_turn_t1 * tmp1;
-
-      }
-      x = -0.8 + sphere_middle.x();
-      y += sphere_middle.y() - r;
-#endif
-      // linear von (-0.757359, 0.76392)
-      // bis (0.1, -1.05)
-      float m = (-1.05 - 0.76392)/(0.1 - (-0.757359));
-      float n = -1.05 - 0.1 * m;
-      if(y <= 0.76392) {
-        x = -2.0 + distance + 0.2;
-        y = m*x+n;
-      }
-
-#if 0
-      float d_tmp = (-2.0 + distance) - (-2.0 - (-0.9)); // distance after left cliff
-      // p1(-0.9, 0.8) p2(0.1, -1.05)
-      float m_x = 0.1 - (-0.9);
-      float m_y = -1.05 - 0.8;
-      float travel = sqrt(m_x * m_x + m_y * m_y);
-      float factor = d_tmp / travel;
-      x = 1.1 + (factor * travel);
-      y -= factor * travel;
-#endif
-      //y = -1.214285714 * x * x - 2.821428571 * x - 0.7757142857;
-      // parabel durch (-0.9, 0.8), (-0.6, 0.5), (0.1, -1.05)
+      y = -(27.0/16.0) * x * x - (16.0/5.0) * x - (1141.0/1600.0);
+      // (-0.9,0.8) > (-0.7,0.7) > (0.1,-1.05)
     }
-#if 0
     if (x == 0.1) {
-      y = -1.05 - (fac * 0.15 * r);
+      // y = -1.05 - (fac * 0.15 * r);
+      y = -1.05;
     }
     if(x > 0.1 && x < 0.9) { // 0.1 behind bottom x / right cliff x
-      //x += distance;
-      y = -1.625 * x * x + 3.4375 * x - 1.3775;
-      // parabel durch (0.1, -1.05), (0.6, 0.1), (0.9, 0.4)
+      y = -(35.0/16.0) * x * x + 4 * x - (457.0/320.0);
+      // (0.1,-1.05) > (0.7,0.3) > (0.9,0.4)
     }
     if(x > 0.9 && x < 2.0) {  // rightcliff x / end x
-      //x += distance;
       y = 0.4;  // right cliff y
     }
-#endif
-    //x = -0.6;
-    //y = 0.5;
-    const QMatrix4x4 translate({1.0, 0.0, 0.0, x,   0.0, 1.0, 0.0, y + r,   0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0});
 
-    //unit = unit * translate * rotate * scale;
-    //unit = unit * translate * scale;
-    //return unit;
-    return translate * scale;
+    float radians = -((360 * (x + 2.0))/(2.0 * r * M_PI)) * (M_PI/180.0);  // - so it turns the right way
+    // x -> no rotation while scaling at x = 0.1
+    float cosine = cos(radians);
+    float sine = sin(radians);
+
+    const QMatrix4x4 rotate(cosine, -sine, 0.0, 0.0,   sine, cosine, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0);
+    const QMatrix4x4 scale(lambda1, 0.0, 0.0, 0.0,   0.0, lambda2, 0.0, 0.0,   0.0, 0.0, lambda3, 0.0,   0.0, 0.0, 0.0, 1.0);
+    const QMatrix4x4 translate(1.0, 0.0, 0.0, x,   0.0, 1.0, 0.0, y + r,   0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0);
+
+    return translate * rotate * scale;
 }
 
 void Exercise13::drawEnvironment()
